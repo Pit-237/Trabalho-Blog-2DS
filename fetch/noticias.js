2c29d5056540e47d9cf09e2107a8059e8ff4414b
casdean <meduarda6660@gmail.com>
2025-12-04 08:17:16 -0300

    noticias-completo

---
+++ fetch/noticias.js
@@ -0,0 +1,121 @@
+document.getElementById('formNoticias').addEventListener('submit', function (form) {
+    form.preventDefault();
+
+    let formData = new FormData(this);
+
+    fetch('http://localhost/Trabalho-Blog-2DS/CRUD/criar_noticia.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(() => {})
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+            alert('Ocorreu um erro na comunicação com o servidor: ' + error.message);
+        });
+
+    window.location.reload();
+});
+
+let lista = document.getElementById('noticias');
+let categorias = {
+    1: "Tecnologia e Sociedade",
+    2: "Ciência e Inovação",
+    3: "Produtos e Gadgets",
+    4: "Mercados e Negócios Tech"
+};
+
+function getNoticias() {
+    fetch('http://localhost/Trabalho-Blog-2DS/CRUD/listar_noticia.php', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then(response => response.json())
+        .then(response => {
+
+            console.log(response);
+
+            response.noticias.forEach(noticia => {
+
+                let excluir = document.createElement('div');
+                excluir.classList.add('excluir');
+                excluir.innerText = "X";
+                excluir.dataset.id = noticia.id;
+
+                excluir.addEventListener('click', () => {
+                    let id = excluir.dataset.id;
+                    let noticia_excluida = { id: id };
+
+                    fetch("http://localhost/Trabalho-Blog-2DS/CRUD/remover_noticia_por_id.php", {
+                        method: "DELETE",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(noticia_excluida)
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data);
+                            window.location.reload();
+                        });
+                });
+
+                // Montagem dos cards
+                let col = document.createElement('div');
+                col.classList.add('col-sm-12', 'col-lg-4', 'my-3');
+
+                let card = document.createElement('div');
+                card.classList.add('news-card');
+
+                let divImg = document.createElement('div');
+                divImg.classList.add('news-img');
+
+                let img = document.createElement('img');
+                img.src = noticia.imagem;
+                img.alt = noticia.titulo;
+
+                let info = document.createElement('div');
+                info.classList.add('news-info');
+
+                let categoria = document.createElement('span');
+                categoria.classList.add('categoria');
+                categoria.innerText = categorias[noticia.categoria];
+
+                let titulo = document.createElement('h3');
+                titulo.classList.add('titulo');
+                titulo.innerHTML = noticia.titulo;
+
+                let autor = document.createElement('p');
+                autor.classList.add('autor');
+                autor.innerText = noticia.autor;
+
+                let data = document.createElement('p');
+                data.classList.add('data');
+                data.innerText = `Publicado em: ${noticia.data_pub}`;
+
+                let btn = document.createElement('a');
+                btn.classList.add('ler-btn');
+                btn.innerText = "Ler mais";
+                btn.href = `pag_noticias.html?id=${noticia.id}`;
+
+                lista.appendChild(col);
+                col.appendChild(card);
+
+                card.appendChild(divImg);
+                divImg.appendChild(img);
+
+                card.appendChild(excluir); 
+
+                card.appendChild(info);
+                info.appendChild(data);
+                info.appendChild(categoria);
+                info.appendChild(titulo);
+                info.appendChild(autor);
+                info.appendChild(btn);
+            });
+        })
+        .catch(erro => {
+            console.log(erro);
+        });
+}
+
+getNoticias();
